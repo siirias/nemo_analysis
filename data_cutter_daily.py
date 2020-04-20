92e1808ca8c6d0424e7015fd54a9d082162dde5b
Simo-Matti Siiria <simo.siiria@fmi.fi>
2020-04-20 17:59:16 +0300

    converted one data cutter into python, added limits to simultaneus processes to avoid resource hogging

---
+++ data_cutter_daily.py
@@ -0,0 +1,62 @@
+import os
+import re
+import subprocess as sp
+import time
+#all_series=['A001', 'A002', 'A005', 'B001', 'B002', 'B005', 'D001', 'D002', 'D005']
+all_series=['B001']
+all_variables=['SST', 'SSS']
+monthly = True
+one_time_processes =  16
+class ProcessCounter:
+
+    def __init__(self, max_processes):
+        self.max_processes = max_processes
+        self.process_list = []
+        self.time_started = 0.0
+        self.time_elapsed = 0.0
+
+    def add(self, new_process):
+        # adds process, and if there is too many, waits for 
+        # them to finnish, then clears the list.
+        if len(self.process_list) == 0:
+            self.time_started = time.time()
+        self.process_list.append(new_process)
+        if(len(self.process_list)>=self.max_processes):
+            print("Waiting...",len(self.process_list))
+            for p in self.process_list:
+                p.wait()
+            self.process_list = []
+            self.time_elapsed = time.time() - self.time_started
+            self.time_started = time.time()
+            print("Waited {:0.3f} s, wait per process {:0.3f} s".format(\
+                    self.time_elapsed, \
+                    self.time_elapsed/self.max_processes))
+
+processes = ProcessCounter(one_time_processes)
+
+for series in all_series:
+    for variable in all_variables:
+        in_dir='/scratch/project_2001635/siiriasi/smartsea_data/'+series+'/'
+        out_dir='/scratch/project_2001635/siiriasi/smartsea_data/daily_test/'
+        #files=$(ls ${in_dir}*_1d_*_grid_T.nc --color=none)
+        file_type = ".*_1d_.*_grid_T.nc"
+        files = os.listdir(in_dir)
+        files = [f for f in files if re.match(file_type,f)]
+        for in_file in files:
+            print(in_file)
+            year=re.search('_1[dmh]_(....)',in_file).groups()[0]
+            for m in range(12):
+                month = m + 1
+                out_file="{}_{}_{}_{}.nc".format(\
+                        variable, series, year, month)
+                the_command = "ncea -O -v {} {} {}".format(\
+                            variable,\
+                            in_dir+in_file,\
+                            out_dir+out_file)
+                sh_output = sp.Popen(the_command,\
+                                universal_newlines = True,\
+                                stdout =sp.PIPE,\
+                                shell = True)
+                processes.add(sh_output)
+#                ncea  -O -v $variable $in_file $outf&
+#                print("{}\n{}".format(in_file,out_file))
