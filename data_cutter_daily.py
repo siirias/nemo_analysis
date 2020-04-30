3a43f9f7d0a9de62833b009ef00cbdff193701df
Simo-Matti Siiria <simo.siiria@fmi.fi>
2020-04-30 11:58:35 +0300

    generalised data cutter and data_zipper

--- data_cutter_daily.py
+++ data_cutter_daily.py
@@ -2,61 +2,45 @@ import os
 import re
 import subprocess as sp
 import time
+import process_helper as ph
 #all_series=['A001', 'A002', 'A005', 'B001', 'B002', 'B005', 'D001', 'D002', 'D005']
-#all_series=['B001']
-all_series=['A001', 'A002', 'A005', 'B002', 'B005', 'D001', 'D002', 'D005']
-all_series=['D005']
-#all_variables=['SST', 'SSS']
-all_variables=['SBS', 'icethic', 'icecon']
+all_series=['A002', 'A005', 'B002', 'B005', 'D002', 'D005']
+all_series=['D002', 'D005']
+#all_series = ['A001']
+#in_dir_root = '/scratch/project_2001635/siiriasi/smartsea_data/'
+in_dir_root = '/scratch/project_2001635/siiriasi/smartsea_data/forcings/'
+#all_variables=['SST', 'SSS', 'SBS', 'icethic', 'icecon', 'iceuvelo', 'icevvelo', 'snowthic']
+all_variables=['sorunoff']
+#file_type = '.*_1d_.*_grid_T.nc'   # daily data
+#year_search = '_1[dmh]_(....)'     # daily data
+#time_axis = 'time_counter'
+file_type = '.*\.nc'   # river forcing
+year_search = '_y(....)'     # river forcing
+time_axis = 'time'
 monthly = True
-one_time_processes =  16
-class ProcessCounter:
+one_time_processes = 6 
 
-    def __init__(self, max_processes):
-        self.max_processes = max_processes
-        self.process_list = []
-        self.time_started = 0.0
-        self.time_elapsed = 0.0
-
-    def add(self, new_process):
-        # adds process, and if there is too many, waits for 
-        # them to finnish, then clears the list.
-        if len(self.process_list) == 0:
-            self.time_started = time.time()
-        self.process_list.append(new_process)
-        if(len(self.process_list)>=self.max_processes):
-            print("Waiting...",len(self.process_list))
-            for p in self.process_list:
-                p.wait()
-            self.process_list = []
-            self.time_elapsed = time.time() - self.time_started
-            self.time_started = time.time()
-            print("Waited {:0.3f} s, wait per process {:0.3f} s".format(\
-                    self.time_elapsed, \
-                    self.time_elapsed/self.max_processes))
-
-processes = ProcessCounter(one_time_processes)
+processes = ph.ProcessCounter(one_time_processes)
 
 for series in all_series:
     for variable in all_variables:
-        in_dir='/scratch/project_2001635/siiriasi/smartsea_data/'+series+'/'
+        in_dir = in_dir_root+series+'/'
         out_dir='/scratch/project_2001635/siiriasi/smartsea_data/daily_test/'
-        file_type = ".*_1d_.*_grid_T.nc"
         files = os.listdir(in_dir)
         files = [f for f in files if re.match(file_type,f)]
         for in_file in files:
             print(in_file)
-            year=re.search('_1[dmh]_(....)',in_file).groups()[0]
+            year=re.search(year_search,in_file).groups()[0]
             for m in range(12):
                 month = m + 1
+                first_step = int((365.25/12.0)*m)
+                last_step = min(364,int((365.25/12.0)*(m+1)))
                 out_file="{}_{}_{}_{}.nc".format(\
                         variable, series, year, month)
-                the_command = "ncea -O -v {} {} {}".format(\
+                the_command = "ncea -O -v {} -d {},{},{} {} {}".format(\
                             variable,\
+                            time_axis, first_step, last_step,\
                             in_dir+in_file,\
                             out_dir+out_file)
-                sh_output = sp.Popen(the_command,\
-                                universal_newlines = True,\
-                                stdout =sp.PIPE,\
-                                shell = True)
-                processes.add(sh_output)
+                print(the_command)
+                processes.add(the_command)
--- data_zipper.py
+++ data_zipper.py
@@ -7,23 +7,43 @@ import glob
 #NOTE: For some reason the zipping halts with this funciton.
 # I used it just to print the commands so I could copy paste
 # those to prompt....
-processes = ph.ProcessCounter(3)
-stats = ['max','min','mean']
-variables = ['SBS','icethic','icecon']
-in_dir = '/scratch/project_2001635/siiriasi/smartsea_data/daily_test/'
-for var in variables:
-    for stat in stats:
-#        print("Handling {}, {}".format(var,stat))
-        out_dir = '/scratch/project_2001635/siiriasi/smartsea_data/daily_test/{}/'.format(stat)
-#        the_command = "zip -v {}/{}_monthly_{}.zip {}".format(\
-#                    out_dir, var, \
-#                    stat, \
-#                    " ".join(glob.glob("{}/{}*.nc".format(out_dir,var)))) 
-        the_command = "zip -v {}/{}_monthly_{}.zip {}".format(\
-                    out_dir, var, \
-                    stat, \
-                    " {}/{}*.nc".format(out_dir,var)) 
-        print(the_command)
-#        processes.add(the_command)
-processes.wait_all()
+skip_processed = True
+skip_original = False
+if( not skip_processed):
+    processes = ph.ProcessCounter(3)
+    stats = ['max','min','mean']
+    variables = ['SSS', 'SST', 'SBS', 'icethic', 'icecon', 'snowthic', 'icevvelo', 'iceuvelo', 'sorunoff']
+    in_dir = '/scratch/project_2001635/siiriasi/smartsea_data/daily_test/'
+    for var in variables:
+        for stat in stats:
+    #        print("Handling {}, {}".format(var,stat))
+            out_dir = '/scratch/project_2001635/siiriasi/smartsea_data/daily_test/{}/'.format(stat)
+    #        the_command = "zip -v {}/{}_monthly_{}.zip {}".format(\
+    #                    out_dir, var, \
+    #                    stat, \
+    #                    " ".join(glob.glob("{}/{}*.nc".format(out_dir,var)))) 
+            the_command = "zip -v {}/{}_monthly_{}.zip {}".format(\
+                        out_dir, var, \
+                        stat, \
+                        " {}/{}*.nc".format(out_dir,var)) 
+            print(the_command)
+    #        processes.add(the_command)
+    processes.wait_all()
+
+if(not skip_original):
+    stats = ['max','min','mean']
+    variables = ['SSS', 'SST', 'SBS', 'icethic', 'icecon', 'snowthic', 'icevvelo', 'iceuvelo', 'sorunoff']
+    in_dir = '/scratch/project_2001635/siiriasi/smartsea_data/daily_test/'
+    for var in variables:
+        for stat in stats:
+            out_dir = '/scratch/project_2001635/siiriasi/smartsea_data/daily_test/{}/'.format(stat)
+            the_command = "zip -v {}/{}_monthly_{}.zip {}".format(\
+                        out_dir, var, \
+                        stat, \
+                        " {}/{}*.nc".format(out_dir,var)) 
+            print(the_command)
+    #        processes.add(the_command)
+    processes.wait_all()
+    
+
 print("All done!")
